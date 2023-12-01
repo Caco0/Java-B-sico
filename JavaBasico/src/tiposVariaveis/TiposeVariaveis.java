fd318296587752ddce234eb2002514eb44f7aa3b
Rafael Dametto <cacotpds@gmail.com>
2023-12-01 10:44:51 -0300

    Introducing Variables

--- JavaBasico/src/tiposVariaveis/TiposeVariaveis.java
+++ JavaBasico/src/tiposVariaveis/TiposeVariaveis.java
@@ -1,5 +1,14 @@
 package tiposVariaveis;
 
 public class TiposeVariaveis {
-  
+  byte idade = 123;
+  short ano = 2023;
+  int cep = 13484120;
+  long cpf = 2207824862568L;
+  float pi= 3.14F;
+  double salario = 2500;
+
+  short numeroCurto = 1;
+  int numeroNormal = numeroCurto;
+  short numeroCUrto2 = (short) numeroNormal;
 }
